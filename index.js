8a7e3c3bcac970a38cd1887e80b8d229bf8b5650
djisq <141136574+djisq@users.noreply.github.com>
2023-08-10 20:31:19 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function 태풍호감도(){
+    alert('천태풍의 호감도가 올랐습니다.');
+    window.location.href = "index.html";
+}
+
+function 백한호감도(){
+    alert('도백한의 호감도가 올랐습니다.');
+    window.location.href = "index.html";
+}
+
+function 태풍호감도감소(){
+    alert('천태풍의 호감도가 감소했습니다.');
+    window.location.href = "index.html";
+}
+
+function 백한호감도감소(){
+    alert('도백한의 호감도가 감소했습니다.');
+    window.location.href = "index.html";
+}
